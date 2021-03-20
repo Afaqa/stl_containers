030362042587c52dba1d295c0197947a9f05845d
itressa <itressa@student.21-school.ru>
2021-03-20 15:49:34 +0300

    init. Started vector

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+#include "vector.hpp"
+#include <iostream>
+
+int main() {
+    std::vector<int> sv;
+    ft::vector<int>  fv;
+    std::iterator<ft::vector<int>, ft::vector<int>::value_type> iterator;
+}
